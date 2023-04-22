45db998aeb2c9fd8aa5654f04ca454ecd39ebf13
MinYoung <90046611+alszero@users.noreply.github.com>
2023-04-22 22:30:08 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void){
+    int N;
+    int max = 0;
+    float avg = 0;
+    
+    scanf("%d", &N);
+    
+    int score[N];
+    
+    for(int i=0; i < N; i++){
+        scanf("%d", &score[i]);
+        if(max<score[i]){
+            max = score[i];
+        }
+    }
+    
+    for(int i=0; i < N; i++){
+        avg += (float)score[i] / max * 100;
+    }
+    
+    printf("%f\n", avg / N);
+    
+    return 0;
+}
